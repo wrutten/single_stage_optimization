09daebc361e9a877e2c0318344ccb56a9b8f839a
W.J. Rutten <w.j.rutten@tue.nl>
2024-06-07 10:34:33 +0200

    updated plotting routines

--- plot_scan.py
+++ plot_scan.py
@@ -9,16 +9,24 @@ import matplotlib.pyplot as plt
 import matplotlib.colors as cm
 import numpy as np
 from src.my_utils import get_convergence_data
-
+from matplotlib import colormaps
 
 parent_path = str(Path(__file__).parent.resolve())
 scan_name = 'results/QH_BenchmarkScan_2/'
 os.chdir(parent_path)
 rootdir  = os.path.join(parent_path, scan_name)
 
+figsize_sidebyside = (10, 4.5)
+# Make sure that the grid is not plotted over the data.
+plt.rc('axes', axisbelow=True)
+
 # Get convergence data per design point
 ts_data, ss_data = get_convergence_data(rootdir)
 
+# Objective to plot
+objectives_legend = ['J_total', 'J_Quadratic_Flux', 'J_Length', 'J_C-C', 'J_Curvature', 'J_Length_penalty', 'J_MSC', 'J_ALS', 'J_Quasisymmetry', 'J_Aspect'] # Jiota excluded because not in objective!
+ss_objectives = ['J', 'Jf', 'J_length', 'J_CC', 'J_CURVATURE', 'J_LENGTH_PENALTY', 'J_MSC', 'J_ALS', 'Jquasisymmetry', 'Jaspect'] # Jiota excluded because not in objective!
+ts_objectives = ['J', 'Jf', 'J_length', 'J_CC', 'J_CURVATURE', 'J_LENGTH_PENALTY', 'J_MSC', 'J_ALS']
 
 ss_X = []
 ss_f_QS = []
@@ -27,7 +35,7 @@ ss_CC = []
 ss_f_A = []
 ss_msc = []
 ss_curv = []
-ss_J = []
+ss_J_list = [[] for _ in range(len(ss_objectives))] 
 ts_X = []
 ts_f_QS = []
 ts_f_QF = []
@@ -35,12 +43,13 @@ ts_CC = []
 ts_f_A = []
 ts_msc = []
 ts_curv = []
-ts_J = []
-# index,Nfeval,J,Jf,J_length,J_CC,J_CURVATURE,J_LENGTH_PENALTY,J_MSC,J_ALS,Lengths,curvatures,msc,B.n,C-C-Sep
+ts_J_list = [[] for _ in range(len(ss_objectives))] 
 
-# Process data
-two_stage_quasisymmetry = 0.0018388705086775513 # manually extracted the values for BenchmarkScan_1 (same for BenchmarkScan_2) from their respective output.txt. This is independent from n_coils for the two stage approach.
-two_stage_aspect_ratio = 7.000556095458935 # manually extracted the values for BenchmarkScan_1 (same for BenchmarkScan_2) from their respective output.txt. This is independent from n_coils for the two stage approach.
+two_stage_quasisymmetry = 0.0018388705086775513 # manually extracted the values for BenchmarkScan_1 (same for BenchmarkScan_2 and 3) from their respective output.txt. This is independent from n_coils for the two stage approach.
+two_stage_Jiota = -1.1398587496646777
+two_stage_aspect_ratio = 7.000556095458935 # manually extracted the values for BenchmarkScan_1 (same for BenchmarkScan_2 and 3) from their respective output.txt. This is independent from n_coils for the two stage approach.
+two_stage_J_aspect_ratio = 0.0235816763385 # computed by sqrt(aspectratio-7)
+
 for dp in ts_data:
     ts_X.append(dp['ncoils'])
     ts_f_QS.append(two_stage_quasisymmetry)  
@@ -49,7 +58,13 @@ for dp in ts_data:
     ts_CC.append(float(dp['C-C-Sep']))
     ts_msc.append(float(dp['msc'])/dp['ncoils']) # convert to average msc by dividing over ncoils
     ts_curv.append(float(dp['curvatures'])/dp['ncoils']) # convert to average by dividing over ncoils
-    ts_J.append(float(dp['J'])) # to plot any objective
+
+    for i in range(len(ts_objectives)):
+        ts_J_list[i].append(float(dp[ts_objectives[i]])) # to plot any objective
+        # Manually add plasma surface properties
+    ts_J_list[len(ss_objectives)-2].append(two_stage_quasisymmetry)
+    # ts_J_list[len(ss_objectives)-2].append(two_stage_Jiota)
+    ts_J_list[len(ss_objectives)-1].append(two_stage_J_aspect_ratio)
 
     if dp['ncoils'] == 3:
         benchmark_ts_QS = two_stage_quasisymmetry
@@ -58,37 +73,50 @@ for dp in ts_data:
 for dp in ss_data:
     ss_X.append(dp['ncoils'])
     ss_f_QS.append(float(dp['Jquasisymmetry']))
-    ss_f_A.append(np.sqrt(float(dp['Jaspect']))+7) # reconstruct aspect ratio, assuming it is always pushed above the target.
+    ss_f_A.append(float(dp['Jaspect'])) # reconstruct aspect ratio, assuming it is always pushed above the target.
     ss_f_QF.append(float(dp['Jf']))
     ss_CC.append(float(dp['C-C-Sep']))
     ss_msc.append(float(dp['msc'])/dp['ncoils']) # convert to average msc by dividing over ncoils
     ss_curv.append(float(dp['curvatures'])/dp['ncoils']) # convert to average by dividing over ncoils
-    ss_J.append(float(dp['J'])) # to plot any objective
+
+    J_list = [[] for _ in range(len(ss_objectives))]
+    for i in range(len(ss_objectives)):
+        ss_J_list[i].append(float(dp[ss_objectives[i]])) # to plot any objective
 
     if dp['ncoils'] == 3:
         benchmark_ss_QS = float(dp['Jquasisymmetry'])
         benchmark_ss_QF = float(dp['Jf'])
 
 
-# Plot QS and QF over scan range
-fig1, ax1 = plt.subplots()
-ax1.scatter(ss_X,ss_f_QS, marker='x', color='0')
-ax1.scatter(ss_X,ss_f_QF, marker='o', color='0')
-ax1.scatter(ts_X,ts_f_QS, marker='x', color='tab:red')
-ax1.scatter(ts_X,ts_f_QF, marker='o', color='tab:red')
+## Plot QS and QF over scan range
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=figsize_sidebyside, tight_layout=True, sharey=True)
 
-ax1.legend(['Single stage QS', 'Single stage QF', 'Two stage QS', 'Two stage QF', 'Jorge Single stage QS'], loc='center left', bbox_to_anchor=(1, 0.5))
-
-
+ax1.set_title('Single stage')
 ax1.set_yscale('log')
 ax1.set_xlabel('# coils')
-ax1.set_ylabel('value')
-fig1.tight_layout()
+ax1.set_ylabel('Objective')
+ax1.grid(color='0.1', linestyle='-.', linewidth=.5)
+ax1.tick_params(which='both', direction="in")
+
+ax2.set_title('Two stage')
+ax2.set_yscale('log')
+ax2.set_xlabel('# coils')
+# ax2.set_ylabel('Objective') # captured by shared axis
+ax2.grid(color='0.3', linestyle='-.', linewidth=.5)
+ax2.tick_params(which='both', direction="in")
+
+# Scatter the data
+ax1.scatter(ss_X,ss_f_QS, marker='x', color='k')
+ax1.scatter(ss_X,ss_f_QF, marker='+', color='k')
+ax2.scatter(ts_X,ts_f_QS, marker='x', color='k')
+ax2.scatter(ts_X,ts_f_QF, marker='+', color='k')
+
+# Put legend to right side of righternmost plot
+ax2.legend(['J_Quasisymmetry', 'J_Quadratic_Flux'], loc='center left', bbox_to_anchor=(1, 0.5))
 
 fig_name = 'scan_QS-QF.png'
 fig_path = rootdir + fig_name
-fig1.savefig(fig_path)
-# fig1.show()
+fig.savefig(fig_path)
 
 ## benchmark plot
 fig2, ax2 = plt.subplots()
@@ -111,100 +139,80 @@ ax2.legend(['Single stage QS', 'Single stage QF', 'Two stage QS', 'Two stage QF'
 
 ax2.set_yscale('log')
 ax2.set_xlabel('# coils')
-ax2.set_ylabel('value')
+ax2.set_ylabel('Objective')
 fig2.tight_layout()
 
 fig_name = 'benchmark_plot.png'
 fig_path = rootdir + fig_name
 fig2.savefig(fig_path)
 
-## Plot C-C distance
-fig3, ax3 = plt.subplots()
-ax3.scatter(ss_X,ss_CC, marker='x', color='0')
-ax3.scatter(ts_X,ts_CC, marker='x', color='tab:red')
+## Objectives plotting
+for i in range(len(ss_objectives)):
+    figi, axi = plt.subplots(tight_layout=True)
+    axi.scatter(ss_X,ss_J_list[i], marker='x', color='0')
+    axi.scatter(ts_X,ts_J_list[i], marker='x', color='tab:red')
+    axi.legend(['Single stage', 'Two stage'], loc='center left', bbox_to_anchor=(1, 0.5))
 
-ax3.legend(['Single stage', 'Two stage'], loc='center left', bbox_to_anchor=(1, 0.5))
-ax3.axhline(y=0.08, color='k', linestyle='--')
+    axi.set_yscale('log')
+    axi.set_xlabel('# coils')
+    axi.set_ylabel(objectives_legend[i])
+    axi.grid(color='0.3', linestyle='-.', linewidth=.5)
+    axi.tick_params(which='both', direction="in")
 
-# ax3.set_yscale('log')
-ax3.set_xlabel('# coils')
-ax3.set_ylabel('CC-sep [m?]')
-fig3.tight_layout()
+    fig_name = 'Scan_' + objectives_legend[i]  +'.png'
+    fig_path = rootdir + fig_name
+    figi.savefig(fig_path)
 
-fig_name = 'scan_CC-sep.png'
+## Objectives plotting
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=figsize_sidebyside, tight_layout=True, sharey=True)
+
+# Exclude total J to make marker color black. (should be done with a proper colormap)
+ax1.scatter(ss_X,ss_J_list[0], marker='x', color='k')
+ax2.scatter(ts_X,ts_J_list[0], marker='x', color='k')
+for i in range(len(ss_objectives)-1): #Make length one shorter to exclude scatter of total J 
+    ax1.scatter(ss_X,ss_J_list[i+1])
+    ax2.scatter(ts_X,ts_J_list[i+1])
+# Scatter J marker again to put it on top. Workaround to easily make sure legend is still right.
+ax1.scatter(ss_X,ss_J_list[0], marker='x', color='k')
+ax2.scatter(ts_X,ts_J_list[0], marker='x', color='k')
+
+ax1.set_title('Single stage')
+ax1.set_yscale('log')
+ax1.set_xlabel('# coils')
+ax1.set_ylabel('Objective')
+ax1.grid(color='0.1', linestyle='-.', linewidth=.5)
+ax1.tick_params(which='both', direction="in")
+
+# ax1.legend(ss_objectives, loc='center left', bbox_to_anchor=(1, 0.5)) # Can be excluded, as colormap is the same for both subplots
+
+ax2.set_title('Two stage')
+ax2.set_yscale('log')
+ax2.set_xlabel('# coils')
+# ax2.set_ylabel('Objective') # captured by shared axis
+ax2.grid(color='0.3', linestyle='-.', linewidth=.5)
+ax2.tick_params(which='both', direction="in")
+
+ax2.legend(objectives_legend, loc='center left', bbox_to_anchor=(1, 0.5))
+
+fig_name = 'Scan_objectives.png'
 fig_path = rootdir + fig_name
-fig3.savefig(fig_path)
-# fig1.show()
+fig.savefig(fig_path)
 
-## Plot Aspect ratio
-fig4, ax4 = plt.subplots()
-ax4.scatter(ss_X,ss_f_A, marker='x', color='0')
-ax4.scatter(ts_X,ts_f_A, marker='x', color='tab:red')
+# ## Plot curvatures
+# fig5, ax5 = plt.subplots()
+# ax5.scatter(ss_X,ss_J, marker='x', color='0')
+# ax5.scatter(ts_X,ts_J, marker='x', color='tab:red')
 
-ax4.legend(['Single stage', 'Two stage'], loc='center left', bbox_to_anchor=(1, 0.5))
-ax4.axhline(y=7, color='k', linestyle='--')
-
-# ax4.set_yscale('log')
-ax4.set_xlabel('# coils')
-ax4.set_ylabel('Aspect ratio')
-fig4.tight_layout()
-
-fig_name = 'scan_aspect_ratio.png'
-fig_path = rootdir + fig_name
-fig4.savefig(fig_path)
-# fig1.show()
-
-## Plot msc
-fig5, ax5 = plt.subplots()
-ax5.scatter(ss_X,ss_msc, marker='x', color='0')
-ax5.scatter(ts_X,ts_msc, marker='x', color='tab:red')
-
-ax5.legend(['Single stage', 'Two stage'], loc='center left', bbox_to_anchor=(1, 0.5))
-ax5.axhline(y=10, color='k', linestyle='--')
+# ax5.legend(['Single stage', 'Two stage'], loc='center left', bbox_to_anchor=(1, 0.5))
 
 # ax5.set_yscale('log')
-ax5.set_xlabel('# coils')
-ax5.set_ylabel('Mean square curvature')
-fig5.tight_layout()
+# ax5.set_xlabel('# coils')
+# ax5.set_ylabel('J')
+# fig5.tight_layout()
 
-fig_name = 'scan_msc.png'
-fig_path = rootdir + fig_name
-fig5.savefig(fig_path)
-# fig1.show()
-
-## Plot curvatures
-fig5, ax5 = plt.subplots()
-ax5.scatter(ss_X,ss_curv, marker='x', color='0')
-ax5.scatter(ts_X,ts_curv, marker='x', color='tab:red')
-
-ax5.legend(['Single stage', 'Two stage'], loc='center left', bbox_to_anchor=(1, 0.5))
-ax5.axhline(y=10, color='k', linestyle='--')
-
-# ax5.set_yscale('log')
-ax5.set_xlabel('# coils')
-ax5.set_ylabel('Max curvature')
-fig5.tight_layout()
-
-fig_name = 'scan_curvatures.png'
-fig_path = rootdir + fig_name
-fig5.savefig(fig_path)
-# fig1.show()
-
-## Plot curvatures
-fig5, ax5 = plt.subplots()
-ax5.scatter(ss_X,ss_J, marker='x', color='0')
-ax5.scatter(ts_X,ts_J, marker='x', color='tab:red')
-
-ax5.legend(['Single stage', 'Two stage'], loc='center left', bbox_to_anchor=(1, 0.5))
-
-ax5.set_yscale('log')
-ax5.set_xlabel('# coils')
-ax5.set_ylabel('J')
-fig5.tight_layout()
-
-fig_name = 'scan_J.png'
-fig_path = rootdir + fig_name
-fig5.savefig(fig_path)
-# fig1.show()
+# fig_name = 'scan_J.png'
+# fig_path = rootdir + fig_name
+# fig5.savefig(fig_path)
+# # fig1.show()
 
 print('Finished plotting')

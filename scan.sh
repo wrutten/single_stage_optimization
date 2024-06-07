329301bf767f32d3e51271f4d4aef40ef03015cd
W.J. Rutten <w.j.rutten@tue.nl>
2024-06-07 10:34:15 +0200

    reran second scan twice

--- scan.sh
+++ scan.sh
@@ -1,3 +1,13 @@
+mpirun -n 8 ./main.py QH --stage1 --stage2 --single_stage --max_modes 3 --ncoils 1 --lengthbound 3.5
+mpirun -n 8 ./main.py QH --stage1 --stage2 --single_stage --max_modes 3 --ncoils 2 --lengthbound 3.5
+mpirun -n 8 ./main.py QH --stage1 --stage2 --single_stage --max_modes 3 --ncoils 3 --lengthbound 3.5
+mpirun -n 8 ./main.py QH --stage1 --stage2 --single_stage --max_modes 3 --ncoils 4 --lengthbound 3.5
+mpirun -n 8 ./main.py QH --stage1 --stage2 --single_stage --max_modes 3 --ncoils 5 --lengthbound 3.5
+mpirun -n 8 ./main.py QH --stage1 --stage2 --single_stage --max_modes 3 --ncoils 6 --lengthbound 3.5
+mpirun -n 8 ./main.py QH --stage1 --stage2 --single_stage --max_modes 3 --ncoils 7 --lengthbound 3.5
+mpirun -n 8 ./main.py QH --stage1 --stage2 --single_stage --max_modes 3 --ncoils 8 --lengthbound 3.5
+mpirun -n 8 ./main.py QH --stage1 --stage2 --single_stage --max_modes 3 --ncoils 9 --lengthbound 3.5
+mpirun -n 8 ./main.py QH --stage1 --stage2 --single_stage --max_modes 3 --ncoils 10 --lengthbound 3.5
 mpirun -n 8 ./main.py QH --stage1 --stage2 --single_stage --max_modes 3 --ncoils 12 --lengthbound 3.5
 mpirun -n 8 ./main.py QH --stage1 --stage2 --single_stage --max_modes 3 --ncoils 11 --lengthbound 3.5
 
@@ -11,3 +21,6 @@ mpirun -n 8 ./main.py QH --stage1 --stage2 --single_stage --max_modes 3 --ncoils
 mpirun -n 8 ./main.py QH --stage1 --stage2 --single_stage --max_modes 3 --ncoils 8 --lengthbound 3.5
 mpirun -n 8 ./main.py QH --stage1 --stage2 --single_stage --max_modes 3 --ncoils 9 --lengthbound 3.5
 mpirun -n 8 ./main.py QH --stage1 --stage2 --single_stage --max_modes 3 --ncoils 10 --lengthbound 3.5
+mpirun -n 8 ./main.py QH --stage1 --stage2 --single_stage --max_modes 3 --ncoils 12 --lengthbound 3.5
+mpirun -n 8 ./main.py QH --stage1 --stage2 --single_stage --max_modes 3 --ncoils 11 --lengthbound 3.5
+
